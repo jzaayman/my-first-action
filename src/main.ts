e0b8119e29ab3f4190569cd45feea57d56fc159e
Jeffrey Johnson <sffortytwo@gmail.com>
2021-08-31 17:20:11 +0200

    First attempt

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {context} from '@actions/github'
 async function run(): Promise<void> {
   const message = core.getInput('message')
   core.warning(context.repo.owner)
+  core.warning(context.repo.repo)
   core.warning(`${context.payload.pull_request?.number}`)
   core.warning(message)
 }
