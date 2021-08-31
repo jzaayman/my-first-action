add7820e053fd5f5f39c97118bc1f837d8a9cb53
Jeffrey Johnson <sffortytwo@gmail.com>
2021-08-31 17:02:40 +0200

    First attempt

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context} from '@actions/github'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  const message = core.getInput('message')
+  core.warning(context.repo.owner)
+  core.warning(`${context.payload}`)
 }
 
 run()
